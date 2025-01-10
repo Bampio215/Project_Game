79bfa876473eabff41dcd1ac2c554e5eb4085a25
Bampio215 <119579930+Bampio215@users.noreply.github.com>
2025-01-10 16:15:04 +0700

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
     private float xRange = 20;
+    AudioManager audioManager;
+    private Property playerProperty;
 
     public GameObject projectilePrefab;
     private bool delaySpace = false;
 
-    Animator animator;
-
+    // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.Euler(0,0,0);
-        animator = GetComponent<Animator>();
-        animator.SetFloat("Speed_f",1);
-        animator.SetBool("Static_b",true);
+        playerProperty = new Property();
     }
+
+    // Update is called once per frame
     void Update()
     {
-        
-        transform.position = new Vector3(transform.position.x, 0, 0);
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -35,38 +34,28 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
         }
-
-        if(Input.GetKey("a")){
-            transform.rotation = Quaternion.Euler(0,-90,0);
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            animator.SetFloat("Speed_f",1);
-            animator.SetBool("Static_b",false);
-        }else if(Input.GetKey("d")){
-            transform.rotation = Quaternion.Euler(0,90,0);
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            animator.SetFloat("Speed_f",1);
-            animator.SetBool("Static_b",false);
-        }else{
-            transform.rotation = Quaternion.Euler(0,0,0);
-            animator.SetBool("Static_b",true);
-        }
-
         if (Input.GetKeyDown(KeyCode.Space) && !delaySpace)
         {
+            audioManager.playSFX(audioManager.gunshot);
+
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
             delaySpace = true;
-            transform.rotation = Quaternion.Euler(0,0,0);
-            animator.SetBool("IsATK",true);
             StartCoroutine(Reset());
 
-        }else{
-            animator.SetBool("IsATK",false);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerProperty.Speed);
+
     }
     IEnumerator Reset()
     {
         yield return new WaitForSeconds(0.5f);
         delaySpace = false;
+    }
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 }
